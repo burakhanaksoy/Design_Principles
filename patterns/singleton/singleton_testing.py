ad97d7cee9a72e1c50e66b4d74fa8eece7e48e5e
burakhanaksoy <31994778+burakhanaksoy@users.noreply.github.com>
2021-06-07 21:42:12 +0300

    Create singleton_testing.py

---
+++ patterns/singleton/singleton_testing.py
@@ -0,0 +1,55 @@
+import unittest
+
+
+class Singleton(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+
+        if cls not in cls._instance:
+            cls._instance[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
+
+        return cls._instance[cls]
+
+
+class Database(metaclass=Singleton):
+
+    _db = {
+        'Tokyo': 34000000,
+        'Istanbul': 17000000,
+        'NewYork': 23000000,
+        'Osaka': 19000000
+    }
+
+    def __init__(self):
+        print('Loading database...')
+
+    def find_total_population(self, cities):
+
+        result = 0
+        for c in cities:
+            result += self._db[c]
+
+        return result
+
+
+class SingletonTests(unittest.TestCase):
+
+    def test_is_singleton(self):
+        db1 = Database()
+        db2 = Database()
+
+        self.assertEqual(db1, db2)
+
+    def test_total_population(self):
+        db = Database()
+
+        res = db.find_total_population(['Tokyo', 'Osaka'])
+        expected = 34000000 + 19000000
+
+        self.assertEqual(res, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
